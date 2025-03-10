68a49e78e815a4f143a462cf8fbeacade6e4666e
Alyona <olenapav97@gmail.com>
2025-03-10 21:05:52 +0200

    finish

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -56,7 +56,7 @@ export const deleteContactController = async (req, res, next) => {
   const contact = await deleteContact(contactId);
 
   if (!contact) {
-    throw createHttpError(404, 'Student not found');
+    throw createHttpError(404, 'Contact not found');
   }
 
   res.status(204).send();
