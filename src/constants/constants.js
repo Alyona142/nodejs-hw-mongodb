d8b31edefe06bb8d4fd4ef32fd54f0a301cd0ea3
Alyona <olenapav97@gmail.com>
2025-03-14 15:40:45 +0200

    fin

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,4 @@
+export const phoneNumberRegex = /^\+380\d{9}$/;
 export const SORT_ORDER = {
   ASC: 'asc',
   DESC: 'desc',
--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -1,18 +1,16 @@
 import Joi from 'joi';
+import { phoneNumberRegex } from '../constants/constants.js';
 
 export const createContactSchema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
-  age: Joi.number().integer().min(6).max(16).required(),
-  gender: Joi.string().valid('male', 'female', 'other').required(),
-  avgMark: Joi.number().min(2).max(12).required(),
-  onDuty: Joi.boolean(),
+  phoneNumber: Joi.string().pattern(phoneNumberRegex).required(),
+  email: Joi.string().email().allow(null).optional(),
+  contactType: Joi.string().valid('personal', 'work').optional(),
 });
 
 export const updateContactSchema = Joi.object({
   name: Joi.string().min(3).max(20),
+  phoneNumber: Joi.string().pattern(phoneNumberRegex),
   email: Joi.string().email(),
-  age: Joi.number().integer().min(6).max(16),
-  gender: Joi.string().valid('male', 'female', 'other'),
-  avgMark: Joi.number().min(2).max(12),
-  onDuty: Joi.boolean(),
+  isFavourite: Joi.boolean(),
 });
