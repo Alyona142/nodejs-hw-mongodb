da49972ed6f095fc1aa9a562921fbbe9a6b0617f
Alyona <olenapav97@gmail.com>
2025-04-06 15:36:47 +0300

    hmgm

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -6,21 +6,22 @@ export const getAllContacts = async ({
   page = 1,
   perPage = 10,
   sortOrder = SORT_ORDER.ASC,
-  sortBy = '_id,',
+  sortBy = '_id',
+  userId,
 }) => {
   const limit = perPage;
   const skip = (page - 1) * perPage;
 
-  const contactQuery = ContactsCollection.find();
-  const contactCount = await ContactsCollection.find()
-    .merge(contactQuery)
-    .countDocuments();
+  const filter = { userId };
 
-  const contacts = await contactQuery
-    .skip(skip)
-    .limit(limit)
-    .sort({ [sortBy]: sortOrder })
-    .exec();
+  const [contactCount, contacts] = await Promise.all([
+    ContactsCollection.countDocuments(filter),
+    ContactsCollection.find(filter)
+      .skip(skip)
+      .limit(limit)
+      .sort({ [sortBy]: sortOrder })
+      .exec(),
+  ]);
 
   const paginationData = calculatePaginationData(contactCount, perPage, page);
 
