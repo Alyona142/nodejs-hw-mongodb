05a3b91b09adedf74aefc0c4ce80ae27d5b2fbf9
Alyona <olenapav97@gmail.com>
2025-04-07 22:21:34 +0300

    ghf

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import router from './routers/index.js';
 
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 import { UPLOAD_DIR } from './constants/index.js';
 
 const PORT = Number(getEnvVar('PORT', 3000));
@@ -37,6 +38,7 @@ export const setupServer = () => {
   app.use(errorHandler);
 
   app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
