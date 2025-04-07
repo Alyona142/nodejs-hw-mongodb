c6f49dcf6ea83f1752be1782052879e9ce84a228
Alyona <olenapav97@gmail.com>
2025-04-07 22:41:33 +0300

    ghj

--- src/server.js
+++ src/server.js
@@ -30,6 +30,7 @@ export const setupServer = () => {
       },
     }),
   );
+  app.use('/api-docs', swaggerDocs());
 
   app.use(router);
 
@@ -38,7 +39,6 @@ export const setupServer = () => {
   app.use(errorHandler);
 
   app.use('/uploads', express.static(UPLOAD_DIR));
-  app.use('/api-docs', swaggerDocs());
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
