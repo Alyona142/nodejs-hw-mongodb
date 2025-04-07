b5c5b56a25e2700ca936eb9857b0b5fe52a53a28
Alyona <olenapav97@gmail.com>
2025-04-07 22:25:31 +0300

    sev

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -14,33 +14,25 @@ import { saveFileToUploadDir } from '../utils/saveFileToUploadDir.js';
 import { saveFileToCloudinary } from '../utils/saveFileToCloudinary.js';
 import { getEnvVar } from '../utils/getEnvVar.js';
 
-export const getContactsController = ctrlWrapper(async (req, res) => {
+export const getContactsController = async (req, res) => {
   const { _id: userId } = req.user;
   const { page, perPage } = parsePaginationParams(req.query);
   const { sortBy, sortOrder } = parseSortParams(req.query);
 
-  const contactsResponse = await getAllContacts({
-    userId,
+  const contacts = await getAllContacts({
     page,
     perPage,
     sortBy,
     sortOrder,
+    userId,
   });
 
-  const { data: contacts, totalItems, totalPages } = contactsResponse;
-
-  res.json({
+  res.status(200).json({
     status: 200,
     message: 'Successfully found contacts!',
     data: contacts,
-    page,
-    perPage,
-    totalItems,
-    totalPages,
-    hasPreviousPage: page > 1,
-    hasNextPage: page < totalPages,
   });
-});
+};
 
 export const getContactByIdController = ctrlWrapper(async (req, res, next) => {
   const { _id: userId } = req.user;
